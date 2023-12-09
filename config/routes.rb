f80d214231754c41f346ecd1f318ddebc5ca0c3f
Daniyal Malik <daniyaldeveloper07@gmail.com>
2023-12-10 00:18:53 +0500

    Completed the devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  devise_for :admins, skip: [:registration]
+  root 'dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
