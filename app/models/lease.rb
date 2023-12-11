405e6ac0b4ed3714327b7937746d3831a5b0b23b
Daniyal Malik <daniyaldeveloper07@gmail.com>
2023-12-11 22:43:22 +0500

    Added the callback to update property status in case of lease deletion

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,10 +1,24 @@
+# frozen_string_literal: true
+
 class Lease < ApplicationRecord
   belongs_to :property
   belongs_to :tenant, class_name: 'User', foreign_key: 'tenant_id'
   has_many :payments
+
   after_commit :update_property_status, on: %i[create update]
+  after_commit :update_property_status_for_delete, on: :destroy
+
+  private
 
   def update_property_status
-    Property.find_by(id: property_id).update(status: 1)
+    change_property_status(1)
+  end
+
+  def update_property_status_for_delete
+    change_property_status(0)
+  end
+
+  def change_property_status(new_status)
+    property&.update_column(:status, new_status)
   end
 end
